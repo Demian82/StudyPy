c1d4385cad642d6562d2d2dd76110ddd0b7d16c5
동현 노 <rdh03040220@gmail.com>
2025-10-01 10:46:42 +0900

    Output according to global variable value

--- BasicProPy/space_needl.py
+++ BasicProPy/space_needl.py
@@ -1,4 +1,4 @@
-LINE = 4
+SIZE = 2
 
 def main():
     spear()
@@ -9,14 +9,14 @@ def main():
     roof()
 
 def spear():
-    for i in range(1,LINE+1):
-        for j in range(1, LINE*3+1):
+    for i in range(1,SIZE+1):
+        for j in range(1, SIZE*3+1):
             print(' ' , end='')
         print('||')
 
 def roof():
-    for i in range(1,LINE+1):
-        for space in range(1, i * -3 + (LINE*3)+1):
+    for i in range(1,SIZE+1):
+        for space in range(1, i * -3 + (SIZE*3)+1):
             print(' ', end='')
         print('__/', end='')
 
@@ -25,7 +25,7 @@ def roof():
         colon(i-1)
         print('\\__')
     print('|', end='')
-    for comma in range(1, LINE*6+1):
+    for comma in range(1, SIZE*6+1):
         print('"', end='')
     print('|')
         
@@ -35,21 +35,22 @@ def colon(num):
         print(':::', end='')
 
 def cup():
-    for i in range(1, LINE+1):
+    for i in range(1, SIZE+1):
         for space in range(1, i):
             print('  ', end='')
         print('\\_/', end='')
-        for v in range(1, i*-2 + (LINE*3) + 1):
+        for v in range(1, i*-2 + (SIZE*3) + 1):
             print('\\/', end='')
         print('\\_/')
 
 
 def column():
-    for looping in range(1, LINE+1):
-        for i in range(1,LINE+1):
-            for j in range(1, LINE*2+2):
+    for looping in range(1, SIZE+1):
+        for i in range(1,SIZE+1):
+            space = SIZE*2+ 2
+            for j in range(1, space):
                 print(' ', end='')
-            print('|%%||%%|')
+            print('|'+'%%'*int(SIZE/2-1)+'||'+'%%'*int(SIZE/2-1)+'|')
 
 
 main()
